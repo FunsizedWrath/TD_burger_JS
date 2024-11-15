7f07ca0711dfdcd6d20e2e9f3cee6d72deb3bda8
Naimau-del <nemotorres.contact@gmail.com>
2024-11-15 17:24:44 +0100

    Update index.js

--- tdBurgerFactory/index.js
+++ tdBurgerFactory/index.js
@@ -1,26 +1,41 @@
-// Index
-let diapoBurger = new Array();
-diapoBurger[0] = "img/burger1.jpg";
-diapoBurger[1] = "img/burger2.jpg";
-diapoBurger[2] = "img/burger3.jpg";
+let diapoBurger = [
+    "img/burger1.jpg",
+    "img/burger2.jpg",
+    "img/burger3.jpg"
+];
 
-let diapoBurgerText = new Array();
-diapoBurgerText[0] = "Burger 1";
-diapoBurgerText[1] = "Burger 2";
-diapoBurgerText[2] = "Burger 3";
+let diapoBurgerText = [
+    "Burger 1",
+    "Burger 2",
+    "Burger 3"
+];
 
 let imageCurrent = 0;
+let textVisible = false;
+
 function nextDiapo() {
     imageCurrent++;
-    if (imageCurrent > 2) {
+    if (imageCurrent >= diapoBurger.length) {
         imageCurrent = 0;
     }
     document.getElementById("imageDiapo").src = diapoBurger[imageCurrent];
-    document.getElementById("diapoText").innerText = diapoBurgerText[imageCurrent];
+    // Update the text if it is visible
+    if (textVisible) {
+        document.getElementById("diapoText").innerText = diapoBurgerText[imageCurrent];
+    }
 }
 
 setInterval(nextDiapo, 10000);
 
 document.getElementById("imageDiapo").addEventListener("click", function() {
-    document.getElementById("diapoText").innerText = diapoBurgerText[imageCurrent];
+    if (textVisible) {
+        document.getElementById("diapoText").innerText = "";
+        textVisible = false;
+    } else {
+        document.getElementById("diapoText").innerText = diapoBurgerText[imageCurrent];
+        textVisible = true;
+    }
 });
+
+// Initial call to set the first image and hide the text
+nextDiapo();
